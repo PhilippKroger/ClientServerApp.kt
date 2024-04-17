a2a533ac8fd1f72d1661bd9976a75a2ed8718262
PhilippKroger <max.bondarev2005@gmail.com>
2024-04-17 15:57:14 +0300

    realization without retrofit

---
+++ app/src/main/java/com/example/clientserverappkt/BaseModel.kt
@@ -0,0 +1,38 @@
+package com.example.clientserverappkt
+
+import javax.security.auth.callback.Callback
+
+
+class BaseModel(
+    private val jokeService: JokeService,
+    private val manageResources: ManageResources
+): Model<Joke, Error> {
+
+    private var callback: ResultCallback<Joke, Error>?=null
+
+    private val noConnection by lazy  { Error.NoConnection(manageResources) }
+    private val serviceError by lazy { Error.ServiceUnavailable(manageResources) }
+
+    override fun fetch() {
+        jokeService.joke(object : ServerCallback {
+            override fun returnSuccess(data: JokeCloud) {
+                callback?.provideSuccess(data.toJoke())
+            }
+
+            override fun returnError(errorType: ErrorType) {
+                when (errorType) {
+                    ErrorType.NO_CONNECTION -> callback?.provideError(noConnection)
+                    ErrorType.OTHER -> callback?.provideError(serviceError)
+                }
+            }
+        })
+    }
+
+    override fun clear() {
+        callback = null
+    }
+
+    override fun init(resultCallback: ResultCallback<Joke, Error>) {
+        callback = resultCallback
+    }
+}
